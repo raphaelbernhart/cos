8bcbcbf28fe4390451a73484cecd94612eb0166b
Raphael Bernhart <mail@raphaelbernhart.at>
2020-09-15 00:45:08 +0200

    added final Initialization

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-const testing = "das ist kein test"
+import Detector from './helper/Detector.js'
+import ClassMutator from './helper/ClassMutator.js'
 
-document.getElementById("test").innerHTML = testing
+let $cosElements: Array<any> = [];
+
+let options: Record<string, boolean|string|number> = {
+    disabled: false
+}
+
+// COS Initialization
+const init = () => {
+    if(options.disabled) return
+    // Get COS Elements
+    $cosElements = Detector()
+
+    // Event Listeners
+        // Scroll
+    window.addEventListener("scroll", () => {ClassMutator($cosElements)})
+        // Window Resize
+    window.addEventListener('resize', refresh)
+        // Orientation Change
+    window.addEventListener('orientationchange', refresh)
+}
+
+// Refresh COS
+const refresh = (): void => {
+    if(options.disabled) return
+
+    // disable()
+    // init()
+}
+
+// Disabling COS
+const disable = (): void => {
+    // window.removeEventListener("scroll", ClassMutator($cosElements))
+    window.removeEventListener('resize', refresh)
+    window.removeEventListener('orientationchange', refresh)
+}
+
+init()
