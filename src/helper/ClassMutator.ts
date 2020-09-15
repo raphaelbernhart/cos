ebd5cf515b81c9f0d321ddb1c85e236a7f4431b2
Raphael Bernhart <mail@raphaelbernhart.at>
2020-09-15 22:20:43 +0200

    added logic to identify if element is in view

--- src/helper/ClassMutator.ts
+++ src/helper/ClassMutator.ts
@@ -1,5 +1,20 @@
+import { options } from '../index.js'
+
 export default ($cosElements: Array<HTMLElement>): void => {
     $cosElements.forEach(e => {
-        
+        const className: string = e.dataset.cos!;
+        const bounding = e.getBoundingClientRect();
+        if (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        ) {
+            e.classList.add(className);
+        } else {
+            if(!options.addOnce) {
+                e.classList.remove(className);
+            }
+        }
     })
 }
