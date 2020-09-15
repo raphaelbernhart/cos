6fa8c1d3d1bad1ccdc0ba46fac9be9ed99a71ce6
Raphael Bernhart <mail@raphaelbernhart.at>
2020-09-15 22:21:55 +0200

    implemented Options Interface; started update options with custom User Options; changed eventListener

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,59 @@
 import Detector from './helper/Detector.js'
 import ClassMutator from './helper/ClassMutator.js'
 
+import { Options, UserOptions } from './interfaces/optionsInterface'
+
 let $cosElements: Array<any> = [];
 
-let options: Record<string, boolean|string|number> = {
-    disabled: false
+// default options
+let options: Options = {
+    disabled: false,
+    addOnce: true
 }
 
 // COS Initialization
-const init = () => {
+const init = (userOptions?: UserOptions) => {
     if(options.disabled) return
+
+    // Set options
+    if(userOptions) {
+        Object.keys(userOptions).forEach(key => {
+            options[key]
+        })
+    }
+
     // Get COS Elements
     $cosElements = Detector()
 
     // Event Listeners
         // Scroll
-    window.addEventListener("scroll", () => {ClassMutator($cosElements)})
+    window.addEventListener("scroll", () => { ClassMutator($cosElements) })
         // Window Resize
-    window.addEventListener('resize', refresh)
+    window.addEventListener('resize', ()=>{ refresh() })
         // Orientation Change
     window.addEventListener('orientationchange', refresh)
+        // Page Loaded
+    document.addEventListener('DOMContentLoaded', _=>{ refresh() })
 }
 
 // Refresh COS
 const refresh = (): void => {
     if(options.disabled) return
 
-    // disable()
-    // init()
+    disable()
+    init()
+    ClassMutator($cosElements)
 }
 
 // Disabling COS
 const disable = (): void => {
-    // window.removeEventListener("scroll", ClassMutator($cosElements))
+    window.removeEventListener("scroll", () => {ClassMutator($cosElements)})
     window.removeEventListener('resize', refresh)
     window.removeEventListener('orientationchange', refresh)
 }
 
 init()
+
+export {
+    options
+};
